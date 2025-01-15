b77be15f2ec7a8c75bb6b79ba8e092e400f909a3
Hanish Kumar <hanishk616@gmail.com>
2025-01-16 00:41:02 +0530

    added launch screens for ios  and android

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:nivvesh/authentication/signup.dart';
-// import 'package:flutter/services.dart';
 import 'package:nivvesh/constants/themes.dart';
 import 'package:nivvesh/home_page/infrastructure/home_repository.dart';
-import 'package:nivvesh/home_page/screens/bottom_nav.dart';
 import 'package:nivvesh/shared/routes.dart';
 import 'package:nivvesh/shared/service_locator.dart';
 
 Future<void> main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await setupServiceLocator();
-  // await SystemChrome.setPreferredOrientations(
-  //   [DeviceOrientation.portraitUp],
-  // );
   runApp(const MyApp());
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
